0f51613a7d4457ad2552af8031de4edc437f8846
Sergio Gernyak <sergeg1990@gmail.com>
2017-07-16 08:00:27 +0300

    Initial Commit

---
+++ DynamicBindingExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DynamicBindingExample
+//
+//  Created by Sergey Gernyak on 7/16/17.
+//  Copyright © 2017 Brain Amsterdam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DynamicBindingExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DynamicBindingExample
+//
+//  Created by Sergey Gernyak on 7/16/17.
+//  Copyright © 2017 Brain Amsterdam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
